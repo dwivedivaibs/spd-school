46e80be8278a8ceec4dee20cacefdc9bfe58f775
Vaibhav D <vaibhav111dwivedi@gmail.com>
2025-05-05 12:10:09 +0530

    update the uploader and fix the dob issue

--- app/admin/tc_entries.rb
+++ app/admin/tc_entries.rb
@@ -1,37 +1,49 @@
 ActiveAdmin.register TcEntry do
+  permit_params(*TcEntry.column_names.map(&:to_sym)) # Permits all DB columns
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  permit_params :file, :student_name, :father_name, :mother_name, :tc_number, :nationality, :dob
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :file, :is_active]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  index do
+    selectable_column
+    id_column
+    column :tc_number
+    column :student_name
+    column :father_name
+    column :mother_name
+    column :nationality
+    column :sc_st_obc
+    column :dob
+    column :subjects
+    column :result
+    column :last_class
+    column :last_class_result
+    column :qualified_for_promotion
+    column :dues_paid
+    column :ncc_cadets
+    column :last_day_school
+    column :leaving_reason
+    column :total_meeting_attendance
+    column :total_attendance
+    column :general_conduct
+    column :category
+    column :certificate_issue_date
+    column :created_at
+    column :updated_at
+    column :file
+    actions
+  end
 
-  #index
-  # index do
-  #   selectable_column
-  #   id_column
-  #   column "Contect Type", :content_type
-  #   column :description
-  #   column :created_at
-  #   column :updated_at
-  #   actions
-  # end
+  form do |f|
+    f.inputs do
+      TcEntry.column_names.map(&:to_sym).each do |field|
+        next if [:created_at, :updated_at].include?(field)
 
-  #form
-  # form do |f|
-  #   f.inputs do
-  #     f.input :content_type, :label => 'Select Content Type', :as => :select, :collection => Content::CONTENT_TYPES
-  #     f.input :description, as: :ckeditor, :input_html => { :class => "ckeditor" }
-  #   end
-  #   f.actions
-  # end
+        if field == :dob
+          f.input :dob, as: :datepicker, datepicker_options: { year_range: "1950:#{Time.current.year}", max_date: "+0D" }
+        else
+          f.input field
+        end
+      end
+    end
+    f.actions
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_academic_year_range
+    "#{Time.current.year}-#{(Time.current + 1.year).year.to_s.last(2)}"
+  end
 end
